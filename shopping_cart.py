d21ef67b0b7eb334405ef56d0338aa54276b9f61
guagua43 <84482067+guagua43@users.noreply.github.com>
2021-06-12 21:45:11 -0400

    Code

--- shopping_cart.py
+++ shopping_cart.py
@@ -37,4 +37,106 @@ def to_usd(my_price):
 
 # TODO: write some Python code here to produce the desired output
 
-print(products)
+import datetime
+
+
+# 1) capture product ids until we are done
+#(use infinite while loop)
+
+# 2) Perform product lookups to determine what the product's name and price
+
+
+total_price = 0
+selected_ids = []
+while True:
+    selected_id = input("Please select/scan a valid Product ID: ")
+    if selected_id.upper() == "DONE":
+        break
+    else:
+        #lookup the corresponding product!
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
+    
+# 3) ADD date/time
+
+timestamp = datetime.datetime.now()
+ts = timestamp.strftime("%Y-%m-%d %H:%M")
+
+# 4) CALCULATE TAX
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+tax_r = os.getenv("TAX_RATE")
+print(tax_r)
+tax = (float(total_price) * float(tax_r))
+total = (float(total_price) + float(tax))
+
+# 5) Print Receipt
+print("-------------------------------")
+print("WELCOME TO NAN'S GROCERY STORE")
+print("CHECKOUT TIME:", ts)
+print("-------------------------------")
+print("PURCHASED ITEMS:")
+
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    print(str(matching_product["id"]) + " " + matching_product["name"] + " " + str(to_usd(matching_product["price"])))
+
+print("-------------------------------")
+print("SUBTOTAL:", to_usd(float(total_price)))
+print("TAX:", to_usd(float(tax)))
+print("TOTAL:", to_usd(float(total)))
+print("-------------------------------")
+print("THANK YOU! SEE YOU AGAIN SOON!")
+print("-------------------------------")
+
+
+# 5) Send Receipt Email
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+SENDGRID_TEMPLATE_ID = os.getenv("SENDGRID_TEMPLATE_ID", default="OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
+SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
+
+template_data = {}
+template_data["ts"] = ts
+template_data["total"] = to_usd(total)
+
+prd_list = []
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    prd_list.append(matching_product)
+
+template_data["products"] = prd_list
+
+print(template_data)
+
+client = SendGridAPIClient(SENDGRID_API_KEY)
+print("CLIENT:", type(client))
+
+message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS)
+message.template_id = SENDGRID_TEMPLATE_ID
+message.dynamic_template_data = template_data
+print("MESSAGE:", type(message))
+
+try:
+    response = client.send(message)
+    print("RESPONSE:", type(response))
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+except Exception as err:
+    print(type(err))
+    print(err)
